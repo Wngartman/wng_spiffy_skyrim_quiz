4769cf9c171d78de4e3a5bd5c4d7295e99b9fc37
William Gartman <wngartman@gmail.com>
2022-01-05 16:49:01 -0600

    Fixed what I broke finally

--- Assets/script.js
+++ Assets/script.js
@@ -1,76 +1,41 @@
 
-quest1.style.display = "none";
-countdownId.style.display = "none";
+
+var start = document.getElementById("start");
+var question = document.getElementById("question");
 var countdown = document.getElementById("countdownId")
-var question = document.getElementById("question")
-var choice1 = document.getElementById("choice1")
-var choice2 = document.getElementById("choice2")
-var choice3 = document.getElementById("choice3")
-var choice4 = document.getElementById("choice4")
+var questions = document.getElementById("questions")
 var questionArr = ["This is a question", "This is another question"]
+var hide = style="display:none"
 
-// choice1.addEventListener("click", choice1Func)
-
-// choice1 = 69
-
-// function choice1Func() {
-//   choice1 = 5
-//   alert('Thanks for clicking!');
-// }
-
-
-
-
-function firstQuestion() {
-question.textContent = questionArr[0].toString();
-
-choice1.addEventListener("click", function(event) {
-	alert('hello');
-});
-
-}
-
-function secondQuestion() {
-  question.textContent = questionArr[1].toString();
-  var answer = [1]
-  if (choice1 = 1) {
- 
-  }
- 
- }
-
+questions.style.display = "none";
+countdownId.style.display = "none";
 
 function startQuiz() {
-    var start = document.getElementById("start");
-    var question = document.getElementById("quest1");
     
-    if (start.style.display === "none") {
-    start.style.display = "block";
-  } else {
     start.style.display = "none";
-    question.style.display = "block";
-    countdown.style.display = "block";
     countdownTimer()
     firstQuestion()
-  }
+}
+function firstQuestion() {
+    questions.style.display = "block";
 }
 
 function countdownTimer() {
-  var timeLeft = 60;
+    var timeLeft = 60;
 
-  // TODO: Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
-  var timeInterval = setInterval(function () {
-    timeLeft--;
-    countdown.textContent = timeLeft + " seconds remaining"
+    // TODO: Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
+    var timeInterval = setInterval(function () {
+        timeLeft--;
+        countdown.textContent = timeLeft + " seconds remaining"
 
-    if(timeLeft === 0) {
-      countdown.textContent = "";
-      clearInterval(timeInterval);
-      displayMessage();
-    }
-  }, 1000);
+        if (timeLeft === 0) {
+            countdown.textContent = "";
+            clearInterval(timeInterval);
+            displayMessage();
+        }
+    }, 1000);
 }
 
-function subtractTime () {
+function subtractTime() {
 
 }
